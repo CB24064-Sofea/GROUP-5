d40974a0646b1014e50d02bad536b565f37f7ddd
CB24064-Sofea <cb24064@adab.umpsa.edu.my>
2026-06-07 12:44:29 +0800

    config edit

--- module1/config/database.php
+++ module1/config/database.php
@@ -1,17 +1,17 @@
 <?php
+// config.php
+session_start();
+$host = 'localhost';
+$port = 3307;        
+$user = 'root';
+$pass = '';
+$dbname = 'group5';
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "group5";
-
-$conn = mysqli_connect(
-    $host,
-    $user,
-    $pass,
-    $db
-);
-
-if (!$conn) {
-    die("Database Connection Failed : " . mysqli_connect_error());
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Set timezone
+date_default_timezone_set('Asia/Kuala_Lumpur');
+?>
